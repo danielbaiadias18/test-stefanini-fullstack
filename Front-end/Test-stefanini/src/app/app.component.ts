bb473a82f57ef54cc08fa63fd9340126827b0a63
Daniel Baia <daniel_baia_dias@hotmail.com>
2021-07-11 11:55:06 -0300

    form added, delete button in grid of person phone added

--- Front-end/Test-stefanini/src/app/app.component.ts
+++ Front-end/Test-stefanini/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { iPersonPhone } from 'src/models/PersonPhone';
 
 @Component({
@@ -8,13 +9,21 @@ import { iPersonPhone } from 'src/models/PersonPhone';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
   title = 'Test-stefanini';
   readonly apiURL : string;
   personPhones: iPersonPhone[] = [];
   personPhone: iPersonPhone = null;
-  
-  constructor(private http : HttpClient) {
+  form: FormGroup;
+
+  constructor(private http : HttpClient,private fb: FormBuilder) {
     this.apiURL = 'http://localhost:5000';
+    this.form = this.fb.group({
+      businessEntityID: [''],
+      name: [''],
+      phoneNumber: ['', Validators.required],
+      phoneNumberTypeID: ['', Validators.compose([Validators.required, Validators.min(1)])],
+    });
   }
 
   ngOnInit(): void {
@@ -22,8 +31,6 @@ export class AppComponent implements OnInit {
     this.http.get(`${ this.apiURL }/api/personphone`)
     .subscribe((response: any) => {
       this.personPhones = response.data.personPhoneObjects;
-      console.log(this.personPhones, "pps")
-      console.log(response)
     });
 
    
@@ -33,7 +40,16 @@ export class AppComponent implements OnInit {
     this.http.get(`${ this.apiURL }/api/personphone/${phoneNumber}/${phoneNumberTypeID}`)
     .subscribe((response: any) => {
       this.personPhone = response.data.personPhoneObject;
-      console.log(this.personPhone, "personPhonepersonPhonepersonPhonepersonPhonepersonPhone")
+      this.form.controls['phoneNumber'].setValue(this.personPhone.phoneNumber);
+      this.form.controls['businessEntityID'].setValue(this.personPhone.businessEntityID);
+      this.form.controls['name'].setValue(this.personPhone.person.name);
+      this.form.controls['phoneNumberTypeID'].setValue(this.personPhone.phoneNumberTypeID);
+    });
+  }
+
+  salvar(){
+    this.http.post(`${ this.apiURL }/api/personphone`, this.form.value)
+    .subscribe((response: any) => {
       console.log(response)
     });
   }
